2d4365dd5b9aaacb11c000541e6d5668ef54ca74
nhyy93 <yennhii0903@gmail.com>
2024-07-15 23:08:50 +0700

    fix full manager

--- src/components/ManagerSidebar.jsx
+++ src/components/ManagerSidebar.jsx
@@ -2,6 +2,10 @@ import { SolutionOutlined, UserOutlined, AccountBookOutlined, FileDoneOutlined }
 import { Layout, Menu } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
+
+import Logo from "../assets/imgs/logoweb.png";
+import '../css/SiderComponent.css'; 
+
 const { Sider } = Layout;
 
 const ManagerSidebar = () => {
@@ -10,29 +14,26 @@ const ManagerSidebar = () => {
       style={{
         height: "100vh",
         position: "fixed",
-        background: "#000000",
+        background: "#ADD8E6", 
       }}
     >
-      <div
-      >
+      <div className="sider-logo">
         <img
-          src="https://t4.ftcdn.net/jpg/02/39/44/75/360_F_239447528_qWYSonUIwGoNC290SfOBq8Gvy3x8ownV.jpg"
-          alt="logo"
-          style={{ width: "100%", height: "180px" }}
+          src={Logo} alt="Logo"
         />
       </div>
-      <Menu theme='dark' defaultSelectedKeys={["1"]} mode="inline">
-        <Menu.Item key="1" icon={<SolutionOutlined />}>
-          <Link to="/manager/requests">Requests</Link>
+      <Menu theme='light' defaultSelectedKeys={["1"]} mode="inline">
+        <Menu.Item key="1" icon={<SolutionOutlined />} className="sider-menu-item">
+          <Link to="/manager/requests" className="sider-menu-link">Requests</Link>
         </Menu.Item>
-        <Menu.Item key="2" icon={<AccountBookOutlined />}>
-          <Link to="/manager/billing">Billings</Link>
+        <Menu.Item key="2" icon={<AccountBookOutlined />} className="sider-menu-item">
+          <Link to="/manager/billing" className="sider-menu-link">Billings</Link>
         </Menu.Item>
-        <Menu.Item key="3" icon={<UserOutlined />}>
-          <Link to="/manager/staff">Staff</Link>
+        <Menu.Item key="3" icon={<UserOutlined />} className="sider-menu-item">
+          <Link to="/manager/staff" className="sider-menu-link">Staff</Link>
         </Menu.Item>
-        <Menu.Item key="4" icon={<FileDoneOutlined />}>
-          <Link to="/manager/request-approved">Request Approval</Link>
+        <Menu.Item key="4" icon={<FileDoneOutlined />} className="sider-menu-item">
+          <Link to="/manager/request-approved" className="sider-menu-link">Request Approval</Link>
         </Menu.Item>
       </Menu>
     </Sider>
